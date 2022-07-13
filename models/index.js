d31772b457f28f6426ce21da910b5467130436f2
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-07-13 00:13:22 -0700

    update model

--- models/index.js
+++ models/index.js
@@ -8,44 +8,32 @@ User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-  });
-
 Post.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
+
 });
-
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-  });
-
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
-
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'SET NULL'
+
 });
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
   onDelete: 'SET NULL'
+
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
 });
 
 
